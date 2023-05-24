4f8235b2340416b8b9b2c1f27a458eed4bb3aa88
Ethan Beneroff <ethanbeneroff@gmail.com>
2023-05-24 18:45:28 -0400

    creates countdown functions

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
 # your code goes here!
+import time
+
+def countdown(int):
+    timer = int
+    while timer > 0:
+        print(f'{timer} SECOND(S)!')
+        timer -= 1
+    print("HAPPY NEW YEAR!")
+
+countdown(20)
+
+def countdown_with_sleep(int):
+    timer = int
+    while timer > 0:
+        time.sleep(1)
+        print(f'{timer} SECOND(S)!')
+        timer -= 1
+    print("HAPPY NEW YEAR!")
